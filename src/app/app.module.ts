c4f9523d6d7787864c752699480b48c26d951baa
Ilayaraja Selva <ilayarajas@moback.com>
2020-03-25 00:42:38 +0530

    user list table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { BackendApiService } from './services/backend-api.service';
 
 
 @NgModule({
@@ -10,9 +11,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [BackendApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/backend-api.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams, HttpRequest, HttpEvent, HttpHeaders } from '@angular/common/http';
+
+@Injectable()
+export class BackendApiService {
+  private baseUrl = 'http://13.235.99.192:8081';
+  constructor(private http: HttpClient, ) {
+
+  }
+  public getCognitiveTestUsers() {
+
+    const headers = new HttpHeaders(
+      { 'application-key': '0847eb02-7fd8-4e69-bda0-db7e3a454b1b' },
+    );
+    const options = {
+      headers,
+    };
+    const url = this.baseUrl + '/getCognitiveTestUsers';
+    const req = new HttpRequest('GET', url, '', options);
+    return this.http.request(req);
+  }
+}
