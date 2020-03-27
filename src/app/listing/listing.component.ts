f42a25dea5af391351af7f0321498d29b68a3bfb
sabari_anand <sabaria@moback.com>
2020-03-27 12:32:20 +0530

    search filter added

--- src/app/listing/listing.component.ts
+++ src/app/listing/listing.component.ts
@@ -31,7 +31,7 @@ export class ListingComponent implements OnInit {
   searchList(data) {
     if (data && data.length > 0) {
       this.userList = this.userListCopy.filter((x) => {
-        return x.examineeName.indexOf(data) >= 0;
+        return x.examineeName.toLowerCase().indexOf(data.toLowerCase()) >= 0;
       });
     } else {
       this.userList = this.userListCopy;
