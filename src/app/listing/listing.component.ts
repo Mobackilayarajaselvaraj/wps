7b9affaa802110c98ea2e4bf3f27a7f73c165450
sabari_anand <sabaria@moback.com>
2020-05-21 15:33:29 +0530

    sorting order fixed

--- src/app/listing/listing.component.ts
+++ src/app/listing/listing.component.ts
@@ -13,6 +13,7 @@ export class ListingComponent implements OnInit {
   public userList: any[] = [];
   userListCopy: any = [];
   spinnerLoad = false;
+  spinnerLoadcsv = false;
   constructor(private _apiService: BackendApiService, private _route:Router, private toastr: ToastrService) {
 
   }
@@ -26,7 +27,15 @@ export class ListingComponent implements OnInit {
 
   }
 
-
+ueser = [
+  {"id":475,"examineeName":"zday-Test3","examineeId":"Uday-Test3","testName":"CognitiveTestPOC","age":8,"date":"2020-05-21 08:56:16"},
+  {"id":475,"examineeName":"hday-Test3","examineeId":"Uday-Test3","testName":"CognitiveTestPOC","age":1,"date":"2020-05-21 08:56:16"},
+  {"id":475,"examineeName":"zday-Test3","examineeId":"Uday-Test3","testName":"CognitiveTestPOC","age":9,"date":"2020-05-21 08:56:16"},
+  {"id":475,"examineeName":"bday-Test3","examineeId":"Uday-Test3","testName":"CognitiveTestPOC","age":1111,"date":"2020-05-21 08:56:16"},
+  {"id":475,"examineeName":"cday-Test3","examineeId":"Uday-Test3","testName":"CognitiveTestPOC","age":222,"date":"2020-05-21 08:56:16"},
+  {"id":475,"examineeName":"eday-Test3","examineeId":"Uday-Test3","testName":"CognitiveTestPOC","age":33,"date":"2020-05-21 08:56:16"},
+  {"id":475,"examineeName":"dday-Test3","examineeId":"Uday-Test3","testName":"CognitiveTestPOC","age":5,"date":"2020-05-21 08:56:16"},
+]
 
   searchList(data) {
     if (data && data.length > 0) {
@@ -38,42 +47,31 @@ export class ListingComponent implements OnInit {
     }
   }
 
-  reverse(){
-    this.userList.reverse();
-  }
 
-  ascdescExam(val){
-    if(val == 1){
-      this.userList.sort((a, b) => a.examineeName.localeCompare(b.examineeName));
+
+  toggle = false;
+  toggles =false;
+  ascDescOrder(data){
+    this.toggle = !this.toggle;
+
+    if(this.toggle){
+      this.userList.sort((a, b) => parseFloat(a[data]) - parseFloat(b[data]));
     }else{
-      this.userList.sort((a, b) => b.examineeName.localeCompare(a.examineeName));
+      this.userList.reverse()
     }
   }
 
-  ascdescAge(val){
-    if(val == 1){
-      this.userList.sort((a, b) => a.age.toString().localeCompare(b.age.toString()));
+  ascdescOrderT(data){
+    this.toggles = !this.toggles;
+    if(this.toggles){
+      this.userList.sort((a, b) => a[data].toLowerCase() !== b[data].toLowerCase() ? a[data].toLowerCase() < b[data].toLowerCase() ? -1 : 1 : 0);
     }else{
-      this.userList.sort((a, b) => b.age.toString().localeCompare(a.age.toString()));
+      this.userList.reverse()
     }
   }
 
 
-  ascdescTest(val){
-    if(val == 1){
-      this.userList.sort((a, b) => a.testName.localeCompare(b.testName));
-    }else{
-      this.userList.sort((a, b) => b.testName.localeCompare(a.testName));
-    }
-  }
 
-  ascdescDate(val){
-    if(val == 1){
-      this.userList.sort((a, b) => a.date.localeCompare(b.date));
-    }else{
-      this.userList.sort((a, b) => b.date.localeCompare(a.date));
-    }
-  }
 
 
 
@@ -88,8 +86,8 @@ export class ListingComponent implements OnInit {
         const resp = data['body'];
         // console.log(resp);
         if (resp && resp.Users && resp.Users.length > 0) {
-            this.userList = [...this.userList, ...resp.Users];
-            this.userListCopy = [...this.userListCopy, ...resp.Users];
+            this.userList = [...this.userList, ...resp.Users, ...this.ueser];
+            this.userListCopy = [...this.userListCopy, ...resp.Users, ...this.ueser];
         }
       }, (error) => {
         this.spinnerLoad = false;
@@ -105,20 +103,28 @@ export class ListingComponent implements OnInit {
       "examineeId": user.examineeId,
       "testName": user.testName
     }
+    this.spinnerLoadcsv = true;
+    setTimeout(()=>{
+      this.spinnerLoadcsv = false;
+    },10000)
     this._apiService.getCognitiveTestResult(list)
       .subscribe((data) => {
+
         if (data &&  data['body']) {
           const resp = data['body'];
           const a = document.createElement('a');
           a.href = 'data:attachment/csv;charset=utf-8,' + encodeURI(resp);
           a.download = 'CognitiveTestReport.csv';
           a.click();
+          this.spinnerLoadcsv = false;
         }else if(data['body'] == ''){
           this.toastr.error('no data!');
         }else{
           console.log('no data', data)
         }
+
       }, (error) => {
+        this.spinnerLoadcsv = false;
         console.log('error::', error.error.text);
         this.toastr.error('no data!');
       },
